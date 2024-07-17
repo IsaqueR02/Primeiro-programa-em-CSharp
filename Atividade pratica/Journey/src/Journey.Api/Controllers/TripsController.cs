2013c3a4e34b4e1b056b2560bea47980530ad0ca
Isaque Roberto <isaquezulato@gmail.com>
2024-07-17 11:43:25 -0300

    add Aula-01

---
+++ Atividade pratica/Journey/src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,29 @@
+﻿using Journey.Application.UseCases.Trips2.Register;
+using Journey.Communication.Requests;
+using Journey.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterTripJson request)
+        {
+            try {
+                var useCase = new RegisterTripUserCase();
+
+                useCase.Execute(request);
+
+                return Created();
+            } 
+            catch (JourneyException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Atividade pratica/Journey/src/Journey.Application/UseCases/Trips2/Register/RegisterTripUserCase.cs
@@ -0,0 +1,35 @@
+﻿using Journey.Communication.Requests;
+using Journey.Exception.ExceptionsBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journey.Application.UseCases.Trips2.Register;
+public class RegisterTripUserCase
+{
+    public void Execute(RequestRegisterTripJson request)
+    {
+        Validate(request);
+    }
+
+    private void Validate(RequestRegisterTripJson request) 
+    {
+        if (string.IsNullOrWhiteSpace(request.Name))
+        {
+                
+            throw new JourneyException("Nome não pode ser vazio");
+        } 
+        
+        if (request.StartDate.Date < DateTime.UtcNow.Date)
+        {
+            throw new JourneyException("A viagem não pode ser registrada para uma data passada.");
+        }
+
+        if (request.EndDate.Date < request.StartDate.Date)
+        {
+            throw new JourneyException("A viagem deve terminar apos a data de inicio.");
+        }
+    }
+}
